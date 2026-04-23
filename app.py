cedf556b61ca415351a2a41356d04b431e6c5ac9
vaibhav22jul09-byte <vaibhav22jul09@gmail.com>
2026-04-23 20:09:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from helper import check_eligibility, get_election_steps, ask_gemini
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Smart Election Assistant is running!"
+    return """
+    <h1>🗳️ Smart Election Assistant</h1>
+
+    <h3>Check Voting Eligibility</h3>
+    <form action="/eligibility">
+        Age: <input name="age"><br><br>
+        Citizen (yes/no): <input name="citizen"><br><br>
+        <input type="submit" value="Check">
+    </form>
+
+    <h3>Ask AI Question</h3>
+    <form action="/ask">
+        Question: <input name="q"><br><br>
+        <input type="submit" value="Ask">
+    </form>
+
+    <h3>Election Process</h3>
+    <a href="/steps">View Election Steps</a>
+    """
 
 @app.route("/eligibility")
 def eligibility():
